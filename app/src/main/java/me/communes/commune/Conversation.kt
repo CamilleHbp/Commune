9f1292b79dcdd39690344744e1a9bb5e58e13e8a
Camille Baillat <camille.baillat@pm.me>
2018-09-18 16:54:46 +0200

    initial android app

---
+++ app/src/main/java/me/communes/commune/Conversation.kt
@@ -0,0 +1,12 @@
+package me.communes.commune
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Conversation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message)
+    }
+}
---
+++ app/src/main/java/me/communes/commune/ConversationListActivity.kt
@@ -0,0 +1,12 @@
+package me.communes.commune
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ConversationListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conversations)
+    }
+}
