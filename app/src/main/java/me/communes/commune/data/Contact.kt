657e9262df37a39be5c2c57af57f4cf97cef0c89
Camille BAILLAT <camille.baillat@pm.me>
2019-03-20 18:00:05 +0100

    [CLEAN] removing old project to start anew

--- app/src/main/java/me/communes/commune/data/Contact.kt
+++
@@ -1,9 +0,0 @@
-package me.communes.commune.data
-
-import android.database.Cursor
-
-data class Contact(val displayName: String = "Unknown", val phoneNumber: String = "Unknown"){
-    fun getCursor(cursor: Cursor) {
-        cursor.columnNames
-    }
-}
